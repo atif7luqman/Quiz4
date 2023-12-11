0abc802d51e6eddcb571dae83a27c7ebd7f681dd
atif7luqman <atifluqman28@gmail.com>
2023-12-11 23:20:58 +0500

    logo renamed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
             <Nav.Link href="#customers">Customers</Nav.Link>
             <NavDropdown title="Resources" id="collapsible-nav-dropdown" drop="down">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">My action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
